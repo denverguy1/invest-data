3b932e5276c5817221727478c26b4dcdfe2f6c5b
Jeff Fleck <jfleck76@gmail.com>
2017-11-21 09:48:00 -0700

    initial version
    Initial version of invest data website

---
+++ app.js
@@ -0,0 +1,53 @@
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
+    mystocksjs = fs.readFileSync('myStocks.js');
+    simdatajs = fs.readFileSync('simData.js');
+
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        if (req.url === '/myStocks.js') {
+          res.writeHead(200);
+          res.write(mystocksjs);
+          res.end();
+        }
+        else if (req.url === '/simData.js') {
+          res.writeHead(200);
+          res.write(simdatajs);
+          res.end();
+        }
+        else {
+          res.writeHead(200);
+          res.write(html);
+          res.end();
+        }
+    }
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
